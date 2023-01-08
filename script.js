49410ea58e4e00fb9adcc80d9b35de2f9f1359dc
Ben L <veloswim@gmail.com>
2023-01-08 13:49:55 -0800

    added comments for each step in js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+// Calling a function like this with jQuery will wait for the page to render before executing code.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
   let containerDiv = $("#block-div");
   let headerText = $("#currentDay");
   let init = false;
@@ -18,12 +8,19 @@ $(function () {
 
   writeToPage();
 
+  // Adding event listener to .saveBtn to set items to local storage.
   $(".saveBtn").on("click", function () {
     let taskStored = JSON.parse(localStorage.getItem("savedTasks"));
     let saveArr = [];
 
     for (let i = 0; i < 9; i++) {
-      let textInput = containerDiv.children().eq(i).children().eq(1).val();
+      let textInput = containerDiv
+        .children()
+        .eq(i)
+        .children()
+        .eq(1)
+        .val()
+        .trim();
       saveArr.push(textInput);
     }
 
@@ -32,17 +29,15 @@ $(function () {
     writeToPage();
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
+  // Adding function to use in an interval to update the hour each second.
   function updateSeconds() {
     updateBackgrounds();
     return currentHour;
   }
+  // Interval calls updateSeconds function every second
+  setInterval(updateSeconds, 1000);
 
+  // Remove classes we want to set, and then based on what is returned from updateSeconds, add the corrosponding class.
   function updateBackgrounds() {
     for (i = 0; i < 9; i++) {
       let blocks = containerDiv.children().eq(i);
@@ -57,12 +52,11 @@ $(function () {
       }
     }
   }
-  setInterval(updateSeconds, 1000);
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+
+  // Renders text content from local storage to each respective div
   function writeToPage() {
     let taskStored = JSON.parse(localStorage.getItem("savedTasks"));
+    updateBackgrounds();
     if (taskStored == null) {
       console.log("no data yet");
       return;
@@ -78,10 +72,10 @@ $(function () {
       }
     }
   }
-  // TODO: Add code to display the current date in the header of the page.
 
+  // Grabbing today's date to render to the page in the header
   function grabDate() {
-    let today = dayjs().format("dddd MMMM, DD");
+    let today = dayjs().format("dddd, MMMM DD, YYYY");
     headerText.text(today);
   }
 });
